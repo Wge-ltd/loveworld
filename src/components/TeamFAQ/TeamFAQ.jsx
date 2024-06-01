a175dac348503b6612ceaf13bab05856c26592d6
Wge-ltd <95187506+Wge-ltd@users.noreply.github.com>
2024-06-02 00:54:18 +0100

    teamfaq

--- src/components/TeamFAQ/TeamFAQ.jsx
+++ src/components/TeamFAQ/TeamFAQ.jsx
@@ -9,23 +9,23 @@ const TeamFAQsData = [
     },
     {
         q: "Can I change my plan later?",
-        a: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+        a: "Certainly! Loveworld Virtual Internship understands that needs may evolve. You have the flexibility to upgrade or downgrade your plan at any time to better suit your requirements.",
     },
     {
         q: "What is your cancellation policy?",
-        a: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+        a: "Our cancellation policy provides users with the freedom to cancel their subscription at any time. No long-term commitments are required, and cancellation is a hassle-free process. Please review our terms and conditions for more details.",
     },
     {
         q: "Can other info be added to an invoice?",
-        a: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+        a: "Yes, Loveworld Virtual Internship allows for customization of invoices. Users can add additional information, such as specific project details, payment terms, or any other relevant data to tailor the invoice to their needs",
     },
     {
         q: "How does billing work?",
-        a: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+        a: "Billing for Loveworld Virtual Internship is typically done on a recurring basis, depending on your chosen subscription plan. Payments can be made securely through our online platform, and you will receive notifications and invoices to keep you informed about upcoming and past transactions.",
     },
     {
         q: "How do I change my account email?",
-        a: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+        a: "To change your account email, log in to your Loveworld Virtual Internship account and navigate to the account settings. Within the settings, you should find an option to update your email address. Follow the prompts to make the change, and don't forget to verify the new email for security purposes. If you encounter any issues, our support team is available to assist you",
     },
 ];
 
